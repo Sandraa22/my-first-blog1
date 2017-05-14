4b874767d87e80c512c81f72b351ac16a818dffc
sandra <sandra.spiridonovaite@gmail.com>
2017-05-14 14:45:10 +0300

    My Django Girl app, first commit

---
+++ Hello.py
@@ -0,0 +1,46 @@
+#print("Hello world")
+
+ #comment
+ # each function starts with def
+'''
+def first():
+    if 2 < 4 >5:
+        print("True")
+    else: print("Not true")
+    print("Hello world")
+'''
+##########################
+'''def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+ '''
+##########################
+'''
+day = "Pirmadienis"
+def second(day):
+    if day == "Pirmadienis":
+        print("Ne", day)
+    elif day =="Antradienis":
+         print("Ne", day)
+    elif day =="Trečiadienis":
+         print("Ne", day)
+    elif day =="Ketvirtadienis":
+         print("Ne", day)
+    elif day =="Penktadienis":
+         print("Ne", day)
+    elif day =="Šeštadienis":
+         print("Ne", day)
+    else: print("Sekmadienis")
+
+#second("Pirmadienis")
+ '''
+##########################
+#for i in range(1,10):
+#    print(i)
+country = ['Lithuania', 'Belarus', 'Poland', 'Germany']
+for i in country:
+     print(i)
